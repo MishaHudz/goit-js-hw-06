de8437126d760c13ef9a97b9b1207fd79af30408
MishaHudz <misha.hudz@gmail.com>
2023-04-24 08:59:16 +0300

    Fixed task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,10 @@
 const inputFild = document.querySelector("#validation-input");
 // const value = Number(inputFild.attributes[2].value);
-const value = Number(inputFild.getAttribute("data-length"));
+// const value = Number(inputFild.getAttribute("data-length"));
+const value = Number(inputFild.dataset.length);
 
 inputFild.addEventListener("blur", (event) => {
+  inputFild.classList.remove("valid");
   inputFild.classList.add("invalid");
 
   if (event.currentTarget.value.length === value) {
